1bf2b6a80b21ed721f6f05c1b3728e0eeb1a4c78
Marco Escaleira <marcoescaleira2000@gmail.com>
2020-04-07 13:56:12 +0100

    feat: adding comments with fixed categories

--- comments.js
+++ comments.js
@@ -1,9 +1,46 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const categories = ["cute", "funny", "roast"];
+
 const getComments = () => {
   return "Your comments...";
 };
 
 const addComment = (category, comment) => {
+  const comments = loadComments();
+  
+  if (categories.indexOf(category) !== -1) {
+    comments.push({
+      category,
+      comment
+    });
+    
+    saveComments(comments);
+  } else {
+    console.log("Please choose a valid category");
+    categories.forEach(cat => console.log(`- ${chalk.green(cat)}`))
+  }
+}
 
+const saveComments = (comments) => {
+  try {
+    const dataJSON = JSON.stringify(comments);
+    fs.writeFileSync("comments.json", dataJSON);
+  } catch(e) {
+    console.log("Something went wrong while saving comments");
+  }
+}
+
+const loadComments = () => {
+  try {
+    const dataBuffer = fs.readFileSync("comments.json");
+    const dataJSON = dataBuffer.toString();
+    
+    return JSON.parse(dataJSON);
+  } catch(e) {
+    return [];
+  }
 }
 
 module.exports = {
